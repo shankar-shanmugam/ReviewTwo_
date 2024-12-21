b46994cb3ba44b2529430136d0f478be06d08f7a
shankar s <shankar89479@gmail.com>
2024-12-21 10:24:44 +0530

    snake and ladder start position

---
+++ SnakeAndLadder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReviewTwo
+{
+    internal class SnakeAndLadder
+    {
+        private int playerPosition;
+        private int WINNING_POSITION = 100;
+
+        public SnakeAndLadder()
+        {
+            playerPosition = 0;
+        }
+        public void Start()
+        {
+            Console.WriteLine("Welcome to Snake and Ladder Game!");
+            Console.WriteLine($"Player starting position: {playerPosition}");
+            Console.WriteLine("Press any key to continue...");
+        }
+
+        public int GetPlayerPosition()
+        {
+            return playerPosition;
+        }
+
+        public void SetPlayerPosition(int newPosition)
+        {
+            playerPosition = newPosition;
+        }
+
+    }
+}
