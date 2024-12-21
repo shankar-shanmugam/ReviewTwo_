115e1f75cbfef8eab496006f1a2b8baafa3f9dd2
shankar s <shankar89479@gmail.com>
2024-12-21 12:45:49 +0530

    Added .gitignore and ignored unnecessary files and done use case upto 6

--- SnakeAndLadder.cs
+++ SnakeAndLadder.cs
@@ -7,9 +7,8 @@ namespace ReviewTwo
 {
     internal class SnakeAndLadder
     {
-        private int playerPosition;
-     
-
+        private static int playerPosition;
+        private static int Count;
         public SnakeAndLadder()
         {
             playerPosition = 0;
@@ -19,45 +18,48 @@ namespace ReviewTwo
         {
             Console.WriteLine("Welcome to Snake and Ladder Game!");
             Console.WriteLine($"Player starting position: {playerPosition}");
-          
 
-            Console.WriteLine("player rolling the dice !!!!");
+            while (playerPosition < 100)
+            {
 
-            int dice_no = Rolldice();
-            Console.WriteLine("Player getting the dice as "+dice_no);
+                Console.WriteLine("player rolling the dice !!!!");
 
-            int optionGetByPlayer = Options();
+                int dice_no = Rolldice();
+                Count++;
+                Console.WriteLine("Player getting the dice as " + dice_no);
 
-            switch (optionGetByPlayer)
-            { 
-                
-                case 0:
+                int optionGetByPlayer = Options();
 
-                    Console.WriteLine("player getting option as no play ");
+                switch (optionGetByPlayer)
+                {
+                    case 0:
 
-                    Console.WriteLine("current player position is "+playerPosition);
-                    break;
-                
-                case 1:
+                        Console.WriteLine("player getting option as no play ");
+                        Console.WriteLine("current player position is " + playerPosition);
+                        break;
 
-                    Console.WriteLine(" player getting ladder ");
-                    int set = dice_no + GetPlayerPosition();
-                    SetPlayerPosition(set);
-                    Console.WriteLine("current player position is " + playerPosition);
-                    break;
+                    case 1:
 
-                case 2:
-                    Console.WriteLine(" player getting snake ");
-                    int set1 = GetPlayerPosition() - dice_no;
-                    if (set1 > 0)
-                    SetPlayerPosition(set1);
-                    Console.WriteLine("current player position is " + playerPosition);
-                    break;
-            
-            
+                        Console.WriteLine(" player getting ladder ");
+                        int set = dice_no + GetPlayerPosition();
+                        if(set > 100) SetPlayerPosition(GetPlayerPosition());
+                        else SetPlayerPosition(set);
+                        Console.WriteLine("current player position is " + playerPosition);
+                        break;
+
+                    case 2:
+                        Console.WriteLine(" player getting snake ");
+                        int set1 = GetPlayerPosition() - dice_no;
+                        if (set1 > 0)
+                            SetPlayerPosition(set1);
+                        else
+                            SetPlayerPosition(0);
+                        Console.WriteLine("current player position is " + playerPosition);
+                        break;
+                }
+
             }
-
-
+            Console.WriteLine("total no of times the dice rolled is " + Count);
         }
 
         public int Rolldice()
@@ -65,18 +67,15 @@ namespace ReviewTwo
             Random r= new Random();
              return  r.Next( 1,7 );
         }
-
         public int Options()
         {
             Random r = new Random();
             return r.Next(0,3);
         }
-
         public int GetPlayerPosition()
         {
             return playerPosition;
         }
-
         public void SetPlayerPosition(int newPosition)
         {
             playerPosition = newPosition;
